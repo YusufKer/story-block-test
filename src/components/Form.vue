912c1f1c1bbad7391f23a48998c501388c4620ad
= <yus.ker1994@gmail.com>
2023-05-22 13:31:41 +0200

    fixed url

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,14 +4,15 @@
         <p>{{ blok.description }}</p>
         <form ref="form" class="max-w-[500px] grid gap-4">
             <div v-for="(inputGroup, index) in blok.input_groups" class="input-group grid gap-1 border">
-                <!-- CHECK WITH DEVON ABOUT WAYS TO GENERATE A UNIQUI ID -->
                 <label 
                     v-if="inputGroup.label" 
+                    :title="inputGroup.description"
                     :for="'input_' + index"
                 >
                 {{ inputGroup.label }}:
                 </label>
                 <input
+                    :name="inputGroup.label"
                     :id="'input_' + index"
                     :type="inputGroup.input_type" 
                     :placeholder="inputGroup.placeholder ? inputGroup.placeholder : ''"
@@ -29,9 +30,21 @@
     const props = defineProps({blok:Object});
     const form = ref();
 
-    function submit(){
+    async function submit(){
         const inputs = form.value.querySelectorAll('input');
-        inputs.forEach(input => console.log({input:input.value}))
+        const object = {};
+        inputs.forEach(input => object[input.name] = input.value);
+        await fetchdata();
+    }
+
+    async function fetchdata(){
+        try{
+            const response = await fetch(props.blok.endpoint.url);
+            const data = await response.json();
+            console.log(data);
+        }catch(error){
+            console.log(error)
+        }
     }
 </script>
 
